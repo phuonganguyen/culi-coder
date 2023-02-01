e27274975896c88be5e786f8853ef9115f110427
alvin.nguyen <alvin.nguyen@luminetsoft.com>
2023-02-01 22:58:43 +0700

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import headerNavLinks from "@/data/headerNavLinks";
+import siteMetadata from "@/data/siteMetadata";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
-import Typewriter from "typewriter-effect";
 
 import Link from "./Link";
 import MobileNav from "./MobileNav";
@@ -10,35 +11,30 @@ import ThemeSwitch from "./ThemeSwitch";
 const Header = () => {
   const router = useRouter()
   return (
-    <header className="flex items-center justify-between py-6">
-      <div>
-        <Link href="/" aria-label="Einar Gudni's website">
-          <div className="flex items-center justify-between text-xl font-semibold text-primary-color dark:text-primary-color-dark">
-            {`~${router.asPath}`}{' '}
-            <Typewriter
-              options={{
-                strings: [],
-                autoStart: true,
-                loop: true,
-              }}
-            />
+    <header className="sticky top-0 left-0 right-0 z-[110] border-b border-gray-200 bg-opacity-30 py-6 backdrop-blur-lg backdrop-filter dark:border-gray-700">
+      <div className="mx-auto flex max-w-3xl items-center justify-between px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+        <Link href="/" aria-label={siteMetadata.title}>
+          <div className="flex items-center justify-between">
+            <div className="mr3">
+              <Image src={'/static/logo.png'} height={35} width={35} />
+            </div>
           </div>
         </Link>
-      </div>
-      <div className="flex items-center text-base leading-5">
-        <div className="hidden sm:block">
-          {headerNavLinks.map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:dark:text-primary-color-dark hover:text-primary-color"
-            >
-              {link.title}
-            </Link>
-          ))}
+        <div className="flex items-center text-base leading-5">
+          <div className="hidden sm:block">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:dark:text-primary-color-dark hover:text-primary-color"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+          <ThemeSwitch />
+          <MobileNav />
         </div>
-        <ThemeSwitch />
-        <MobileNav />
       </div>
     </header>
   )
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,14 +9,16 @@ type Props = {
 }
 
 const Layout = ({ children }: Props) => (
-  <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
-    <div className="flex flex-col justify-between h-screen">
-      <Header />
-      <main className="relative mb-auto">{children}</main>
-      <NowPlaying />
-      <Footer />
+  <>
+    <Header />
+    <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
+      <div className="flex flex-col justify-between h-screen">
+        <main className="relative mb-auto">{children}</main>
+        <NowPlaying />
+        <Footer />
+      </div>
     </div>
-  </div>
+  </>
 )
 
 export default Layout
